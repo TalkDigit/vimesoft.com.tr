feb4eb2b4519a1b8207559916d2292ebf1422403
imGrappa <koray_han_1903@hotmail.com>
2021-11-18 12:13:10 +0300

    güncel revizeler

--- js/script.js
+++ js/script.js
@@ -292,6 +292,42 @@ $(function () {
 });
 
 
+// Gsm +90 mask
+var phoneInput = document.querySelector('#phone') 
+ 
+
+phoneInput.addEventListener('keydown', function(event) { 
+ 
+
+   if( !(event.key == 'ArrowLeft' || event.key == 'ArrowRight' || event.key == 'Backspace' || event.key == 'Tab')) { event.preventDefault() } 
+ 
+
+    var mask = '+90 (111) 111-11-11';   
+ 
+
+    if (/[0-9\+\ \-\(\)]/.test(event.key)) { 
+
+        var currentString = this.value; 
+        var currentLength = currentString.length; 
+		
+        if (/[0-9]/.test(event.key)) { 
+            if (mask[currentLength] == '1') { 
+                this.value = currentString + event.key; 
+            } else { 
+                for (var i=currentLength; i<mask.length; i++) { 
+                if (mask[i] == '1') { 
+                    this.value = currentString + event.key; 
+                    break; 
+                } 
+                currentString += mask[i]; 
+                } 
+            } 
+        } 
+    }  
+}); 
+
+
+
 
 
 $('.s-detail, .share-calendar-link').on('click', function(e) {
